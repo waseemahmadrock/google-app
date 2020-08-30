7541a87b14567d774f2171c66e330ef17cf976e1
waseemahmadrock <waseemahmadrock@gmail.com>
2020-08-30 06:52:20 +0500

    Updates

---
+++ precache-manifest.0706e6f09bedf349350a10926825aa2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46069599975d053e647a667773897402",
+    "url": "/google-app/index.html"
+  },
+  {
+    "revision": "a080c851c790dbf54a03",
+    "url": "/google-app/static/css/main.7259b849.chunk.css"
+  },
+  {
+    "revision": "4528e192c4c351611c49",
+    "url": "/google-app/static/js/2.c07255dd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/google-app/static/js/2.c07255dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a080c851c790dbf54a03",
+    "url": "/google-app/static/js/main.441b1e39.chunk.js"
+  },
+  {
+    "revision": "ef434421319ecd42c77e",
+    "url": "/google-app/static/js/runtime-main.b0c63c24.js"
+  }
+]);
